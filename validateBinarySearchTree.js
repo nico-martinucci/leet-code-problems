c09b1a19c0d2102893125a9246ed60e8fd811820
Nico Martinucci <domenico.martinucci@gmail.com>
2023-03-11 14:02:20 -0800

    add validateBinarySearchTree problem

---
+++ validateBinarySearchTree.js
@@ -0,0 +1,82 @@
+/*
+Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+A valid BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ 
+
+Example 1:
+Input: root = [2,1,3]
+Output: true
+
+Example 2:
+Input: root = [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+ 
+
+Constraints:
+The number of nodes in the tree is in the range [1, 104].
+-231 <= Node.val <= 231 - 1
+
+NOTES:
+- need to make sure to evaluate nodes in the context of the entire side of the
+    tree/subtree, not just it's parent node
+- we care about any given node's relationship to its parent and grandparent, 
+    which can be one of four options:
+        - left/left - needs to be less than both
+        - right/right - needs to be greater than both
+        - G.left -> P.right -> C - needs to be greater than P, less than G
+        - G.right -> P.left -> C - needs to be less than P, greater than G
+- special cases: root (or no parent node) can just BE; first children (or no 
+    grandparent node) only have to evaluate to their parent
+- pass: values and left/right pathway to get to this point for both parent and grandparent
+
+APPROACH:
+- dfs/recursive; pass objects with pathway and value from ancestors
+
+- if node is null, return true
+- evaluate node in context of its ancestors
+    - check node's value against parent and grandparent, depending on pathway
+        - if P -> C is left and C is greater than P, return false
+        - if P -> C is right and C is less than P, return false
+        - if G -> P is left and C is greater than G, return false
+        - if G -> P is right and C is less than G, return false
+    - if we make it here, current node itself is valid
+- then recursively call on its children nodes
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function (root, parent = null, grandparent = null) {
+    if (root === null) return true;
+
+    if (parent) {
+        if (parent.left === root && root.val >= parent.val) return false;
+        if (parent.right === root && root.val <= parent.val) return false;
+    }
+    if (grandparent) {
+        if (grandparent.left === parent && root.val >= grandparent.val) return false;
+        if (grandparent.right === parent && root.val <= grandparent.val) return false;
+    }
+
+    const leftValid = isValidBST(root.left, parent = root, grandparent = parent);
+    const rightValid = isValidBST(root.right, parent = root, grandparent = parent);
+
+    if (leftValid && rightValid) return true;
+
+    return false;
+};
